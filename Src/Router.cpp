aa10182b1491b7b06bd560f7c944cd283ae1fc19
AryanSoltan <arysoltani@gmail.com>
2022-06-27 14:48:50 +0430

    Red protocol added

--- Src/Router.cpp
+++ Src/Router.cpp
@@ -71,6 +71,120 @@ void Router::add_new_packets(std::vector<Packet> new_packets)
 	}
 }
 
+void RouterRed::calculate_avg()
+{
+	new_avg = (1 - w_q) * new_avg + w_q * router_queue.size();
+}
+
+double RouterRed::calculate_new_probablility()
+{
+	if(new_avg <= min_th)
+	{
+		return 1;		
+	}
+	if(new_avg >= max_th)
+	{
+		return 0;
+	}
+	return 0.5 * ((new_avg - min_th) / (max_th - min_th));
+}
+
+void RouterRed::add_new_packets_red(std::vector<Packet> new_packets)
+{
+	for(auto packet : new_packets)
+	{
+		double prob = calculate_new_probablility();
+		std::cerr << "Probability for packet is: " << prob << std::endl;
+		std::cerr << "Average packet is: " << new_avg << std::endl;
+		if(prob > 0.5)
+		{
+			std::cerr << "From Router packet sent " << std::endl;
+			router_queue.push(packet);
+		}
+		else 
+		{
+			std::cerr << "Packet drop due to RED protocl" << std::endl;
+		}
+		calculate_avg();
+	}
+}
+
+void RouterRed::run_recv_thread_red()
+{
+	fd_set master_set, working_set;
+
+	int max_fd = router_fd;
+
+	FD_ZERO(&master_set);
+	FD_SET(router_fd, &master_set);
+
+	while(true)
+	{
+		char buf[MAX_DATA_SIZE];
+		working_set = master_set;
+		if (select(max_fd + 1, &working_set, NULL, NULL, NULL) == -1)
+		{
+			std::cout << "Select failed" << std::endl;
+			exit(-1);
+		}
+
+		if (FD_ISSET(router_fd, &working_set))
+		{
+			int new_fd = accept_new_request(router_fd);
+
+			hosts.push_back(new_fd);
+			number_messages_from_host.push_back(0);
+			max_fd = std::max(max_fd, new_fd);
+			FD_SET(new_fd, &master_set);
+		}
+		else
+		{
+			int len = hosts.size();
+			for (int i = 0; i < len; i++)
+			{
+				if (!FD_ISSET(hosts[i], &working_set))
+					continue;
+				int msg_size = recv(hosts[i], buf, LEN_PACKET, 0);
+				if(msg_size <= 0)
+				{
+					if(msg_size < 0)
+						std::cout << "Message is broken" << std::endl;
+					else if(msg_size == 0)
+					{
+						std::cout << "A client quits" << std::endl;
+						FD_CLR(hosts[i], &master_set);
+						number_messages_from_host[i] = 0;
+						close(hosts[i]);
+						hosts.pop_back();
+					}
+				}
+				else{
+					buf[msg_size] = '\0';
+
+					if(!number_messages_from_host[i])
+					{
+						name_host[i] = buf[0];
+						std::cerr << "New Host name is " << buf[0] << std::endl;
+					}
+					else 
+					{
+						std::cerr << "msg length : " << msg_size << std::endl;
+						std::cerr << "char first packet : reciver, sender: " <<  (byte)buf[0] << " " <<  (byte)buf[2] << " " << std::endl;
+						std::cerr <<"show________________" << std::endl;
+						for (int i = 0; i < msg_size; i++)
+							std::cerr << buf[i];
+						std::cerr <<"______________________" << std::endl;
+						
+						std:: vector<Packet> new_packets = PacketTool::parse_packet(buf, msg_size);
+						add_new_packets_red(new_packets);
+					}
+					number_messages_from_host[i]++;
+				}
+			}
+		}
+	}
+}
+
 void Router::run_recv_thread()
 {
 	fd_set master_set, working_set;
@@ -167,15 +281,61 @@ Router::~Router ()
 	close(router_fd);
 }
 
+RouterRed::RouterRed(int len_input)
+	:Router(len_input)
+{
+		w_q = 0.002;
+		max_th =  len_input;
+		min_th = 0.3 * len_input;
+}
+
+void RouterRed::run_send_thread_red()
+{
+	while(true)
+	{
+		if(router_queue.size())
+		{
+			Packet packet = router_queue.front();
+			router_queue.pop();
+
+			int fd_need = find_host(packet.reciver);
+			Msg msg(packet);
+			send(fd_need, msg.msg, msg.len, 0);
+		}
+	}	
+}	
+
+
+void RouterRed::run_red()
+{
+	pthread_t threads[2];
+	int ret_code;
+
+	std::thread thread1(&RouterRed::run_recv_thread_red, this);
+	std::thread thread2(&RouterRed::run_send_thread_red, this);
+	thread1.join();
+	thread2.join();
+}
+
 int main(int argc, char* argv[])
 {
-	if (argc != 2)
+	if (argc != 3)
 	{
-		std::cout << "Wring number of Argumants for running Router" << std::endl;
+		std::cout << "Wrong number of Argumants for running Router" << std::endl;
 		return 0;
 	}
 	std::string queue_size(argv[1]);
-	Router* router = new Router(stoi(queue_size));
-	router->run_all();
-	delete router;
+	if(argv[2][0] == '0')
+	{
+		Router* router = new Router(stoi(queue_size));
+		router->run_all();
+		delete router;
+	}
+	else 
+	{
+		RouterRed* router_red = new RouterRed(stoi(queue_size));
+		router_red->run_red();
+		delete router_red;
+	}
+
 }
